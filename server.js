ab0c1e64342cfd35301748134e5066d6942be095
Jose F. Romaniello <jfromaniello@gmail.com>
2012-12-28 10:30:16 -0300

    fix login button

--- server.js
+++ server.js
@@ -36,9 +36,6 @@ app.get('/', function (req, res) {
   if (!req.user){
     auth0Client.getConnections(function (err, connections) {
       res.render("index", {
-        user: null,
-        clientID: process.env.clientId,
-        namespace: process.env.namespace,
         connections: connections.map(function(c){
           return c.name;
         })
